7691c8715af55c376fb7acce49be8b233e1c3ca9
afifrivay29 <afifrivaykusnanto29@gmail.com>
2025-06-07 18:09:20 +0700

    belajar jsx

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -1,5 +1,21 @@
 function Article() {
-  return <div>Ini adalah komponen pertama</div>;
+  const name = "Afif";
+  const titles = ["Tutorial React.JS", "Tutorial Node.JS", "Tutorial Next.JS"];
+  return (
+    <>
+      <div>Ini adalah komponen pertama</div>
+      <p>Author: {name}</p>
+      <div>
+        {titles.map((title) => {
+          return (
+            <>
+              <div>{title}</div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Article;
