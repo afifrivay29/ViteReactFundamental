a313e4ebdba12a4739512e408c60e5ae9ca157c2
afifrivay29 <afifrivaykusnanto@gmail.com>
2025-07-14 21:06:11 +0700

    Create onClick Methods

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,14 +2,24 @@ import { useState } from "react"
 
 function Search(props) {
     const [search, setSearch] = useState("")
-    const changeSearch = (event) => {
-        setSearch(event.target.value)
-        props.onSearchChange(event.target.value)
+
+    const changeSearch = () => {
+        props.onSearchChange(search)
     }
+
+    const handleKeydown = (e) => {
+        if(e.key === "Enter") {
+            changeSearch()
+        }
+    }
+
     return (
         <>
-            <div>cari artikel : <input onChange={changeSearch} /></div>
-            <small>ditemukan {props.totalPost} data dengan pencarian kata {search}</small>
+            <div>cari artikel : 
+                <input onChange={(e) => setSearch(e.target.value)} onKeyDown={handleKeydown}/>
+                <button onClick={changeSearch}>Cari</button>
+            </div>
+            <small>ditemukan {props.totalPost} data dengan pencarian kata... "{search}"</small>
         </>
     )
 }
